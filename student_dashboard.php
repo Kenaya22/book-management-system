cc49575e705d2991f86da9924d156d46ad4783e8
Kenaya22 <118979839+Kenaya22@users.noreply.github.com>
2025-04-05 15:27:38 +0800

    Update student_dashboard.php

--- student_dashboard.php
+++ student_dashboard.php
@@ -2,7 +2,7 @@
 // Student dashboard
 session_start();
 if ($_SESSION['role'] != 'student') exit("Access denied.");
-$conn = new mysqli("localhost", "root", "", "book_management");
+$conn = new mysqli("db4free.net", "bookuser", "book122003", "bookdbpunzalan");
 $id = $_SESSION['user_id'];
 
 $result = $conn->query("SELECT b.title FROM books b
