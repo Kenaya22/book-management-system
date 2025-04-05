89e2f53b8c5bc59443fc2e9465dc51a03c5b950e
Kenaya22 <118979839+Kenaya22@users.noreply.github.com>
2025-04-05 15:27:54 +0800

    Update teacher_dashboard.php

--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -2,7 +2,7 @@
 // Teacher dashboard
 session_start();
 if ($_SESSION['role'] != 'teacher') exit("Access denied.");
-$conn = new mysqli("localhost", "root", "", "book_management");
+$conn = new mysqli("db4free.net", "bookuser", "book122003", "bookdbpunzalan");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['title'])) {
     $stmt = $conn->prepare("INSERT INTO books (title, description, cover_image) VALUES (?, ?, ?)");
